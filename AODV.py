06a5707162931ea90309caf63da908d4898516cf
s0578026 <kay.birth@student.htw-berlin.de>
2022-12-19 16:35:18 +0100

    implemented generate and process RREP,RREQ

--- AODV.py
+++ AODV.py
@@ -1,37 +1,23 @@
 import base64
 import bitstring
 import re
+from time import time
 from queue import Queue
 from SerComClient import SerCom
 
 class Route:
-    def __init__(self, destinationAdress: str, destinationSequenceNumber: int, isDestinationSequenceNumberValid: bool, hopCount: int, nextHop: str, precursers: list, lifetime: int):
-        self.destinationAdress = destinationAdress
-        self.destinationSeqNum = destinationSequenceNumber
-        self.isDestinationSequenceNumberValid = isDestinationSequenceNumberValid
-        self.hopCount = hopCount
-        self.nextHop = nextHop
-        self.precursers = precursers
-        self.lifetime = lifetime
-
-class RoutingTable:
-    table = {}
-
-    def hasEntryForDestination(self, destination: str) -> bool:
-        return self.table.get(destination) != None
-
-    def getEntry(self, destination: str) -> Route:
-        return self.table.get(destination)
-
-    def addEntry(self, route: Route):
-        self.table[route.destinationAdress] = route
-
-    def updateEntry(self, new: Route):
-        self.table.update({new.destinationAdress : new})
+    destinationAdress = ""
+    destinationSequenceNumber = 0
+    isDestinationSequenceNumberValid = False
+    hopCount = 0
+    nextHop = ""
+    precursers = []
+    lifetime = 0
+    active = False
 
 class RouteRequest:
     type = 1      
-    flagOne = False
+    unknownSequenceNumber = False
     flagTwo = False
     flagThree = False
     flagFour = False
@@ -43,7 +29,7 @@ class RouteRequest:
     destinationSequence = 0
     originatorAdress = "000F"
     originatorSequence = 0
-    format = "uint6=type, bool1=flagOne, bool1=flagTwo, bool1=flagThree, bool1=flagFour, bool1=flagFive, bool1=flagSix, uint6=hopCount, uint6=requestID, hex16=destinationAdress, int8=destinationSequence, hex16=originatorAdress, uint8=originatorSequence"
+    format = "uint6=type, bool1=unknownSequenceNumber, bool1=flagTwo, bool1=flagThree, bool1=flagFour, bool1=flagFive, bool1=flagSix, uint6=hopCount, uint6=requestID, hex16=destinationAdress, int8=destinationSequence, hex16=originatorAdress, uint8=originatorSequence"
 
     def decode(self, msg: str):
         base64_bytes = msg.encode("ascii")
@@ -51,7 +37,7 @@ class RouteRequest:
         byteArray = bitstring.BitArray(bytes=message_bytes)
         arglist = byteArray.unpack(self.format)
         self.type = arglist[0]
-        self.flagOne = arglist[1]
+        self.unknownSequenceNumber = arglist[1]
         self.flagTwo = arglist[2]
         self.flagThree = arglist[3]
         self.flagFour = arglist[4]
@@ -96,104 +82,147 @@ class RouteReply:
         base64string = base64.b64encode(byteArray.tobytes())
         return base64string.decode("ascii")
 
+class RoutingTable:
+    table = {}
+
+    def isUpdatedNeeded(self,currentEntry, newEntry) -> bool:
+        if not newEntry.isDestinationSequenceNumberValid:
+            return True
+        if newEntry.destinationSeqNum-currentEntry.destinationSeqNum < 0:
+            return True
+        if currentEntry.destinationSeqNum == newEntry.destinationSeqNum and newEntry.hopCount < currentEntry.hopCount:
+            return True
+
+    def hasEntryForDestination(self, destination: str) -> bool:
+        return self.table.get(destination) != None
+
+    def getEntry(self, destination: str) -> Route:
+        return self.table.get(destination)
+
+    def updateEntryWithDestination(self,destinationAdress: str):
+        if not self.hasEntryForDestination(destinationAdress):
+            newRoute = Route()
+            newRoute.isDestinationSequenceNumberValid = False
+            newRoute.destinationSequenceNumber = 0
+            newRoute.destinationAdress = destinationAdress
+            newRoute.lifetime = int(time()*1000)+AODV.ACTIVE_ROUTE_TIMEOUT 
+            newRoute.hopCount = 1
+            newRoute.nextHop = destinationAdress
+            newRoute.active = True
+            self.table.update({newRoute.destinationAdress : newRoute})
+
+    def updateEntryWithRREP(self, rrep: RouteReply, previousHopAdress: str):
+        newRoute = Route()
+        newRoute.isDestinationSequenceNumberValid = True
+        newRoute.destinationSequenceNumber = rrep.destinationSequence
+        newRoute.destinationAdress = rrep.destinationAdress
+        newRoute.lifetime = int(time()*1000)+rrep.lifetime
+        newRoute.nextHop = previousHopAdress
+        newRoute.active = True
+        oldRoute = self.getEntry(rrep.destinationAdress)
+        if oldRoute is None or self.isUpdatedNeeded(oldRoute,newRoute):
+            self.table.update({newRoute.destinationAdress : newRoute})
+
+    def updateEntryWithRREQ(self, rreq: RouteRequest, previousHopAdress: str):
+        oldRoute = self.getEntry(rreq.originatorAdress)
+        currentLifetime = 0
+        currentDesitantionSecquenceNumber = 0
+        if oldRoute is not None:
+            currentLifetime = oldRoute.lifetime
+            currentDesitantionSecquenceNumber = oldRoute.destinationSequenceNumber
+        newRoute = Route()
+        newRoute.isDestinationSequenceNumberValid = True
+        newRoute.destinationSequenceNumber = max(currentDesitantionSecquenceNumber,rreq.originatorSequence)
+        newRoute.destinationAdress = rreq.originatorAdress
+        newRoute.lifetime = max(currentLifetime, int(time()*1000)+ 2*AODV.NET_TRAVERSAL_TIME - 2*rreq.hopCount* AODV.NODE_TRAVERSAL_TIME)
+        newRoute.nextHop = previousHopAdress
+        newRoute.hopCount = rreq.hopCount
+        newRoute.active = True
+        if oldRoute is None or self.isUpdatedNeeded(oldRoute,newRoute):
+            self.table.update({newRoute.destinationAdress : newRoute})
+
+    def updateEntry(self, route: Route):
+        self.table.update({route.destinationAdress : route})
+
 class AODV:
     routingTable = RoutingTable()
     reverseRoutingTable = RoutingTable()
     lastAdress = None
     ownAdress = None
     sequenceNumber = 0
+    lastRequestID = 0
     inputQueue = Queue(maxsize=0)
     outputQueue = Queue(maxsize=0)
-    rreqBuffer = Queue(maxsize=0)
+    rreqBuffer = []
     serialPortClient = SerCom(inputQueue,outputQueue)
 
-    ACTIVE_ROUTE_TIMEOUT = 3
-    TIMEOUT_BUFFER = 2 
-    TTL_START = 1 
-    TTL_INCREMENT = 2 
-    TTL_THRESHOLD = 7 
-    TTL_VALUE = None # Lifetime Value of Package
-    ALLOWED_HELLO_LOSS = 2 
-    RREQ_RETRIES = 2 
-    NODE_TRAVERSAL_TIME = 40 
-    NET_DIAMETER = 35
-    NET_TRAVERSAL_TIME = 2 * NODE_TRAVERSAL_TIME * NET_DIAMETER 
-    BLACKLIST_TIMEOUT = RREQ_RETRIES * NET_TRAVERSAL_TIME 
-    DELETE_PERIOD = None #see note in RFC
-    HELLO_INTERVAL = 1
-    LOCAL_ADD_TTL = 2 
-    MAX_REPAIR_TTL = 0.3 * NET_DIAMETER
-    MIN_REPAIR_TTL= None # last known hop count
-    MY_ROUTE_TIMEOUT = 2 * ACTIVE_ROUTE_TIMEOUT 
-    NEXT_HOP_WAIT = NODE_TRAVERSAL_TIME + 10 
-    PATH_DISCOVERY_TIME = 2 * NET_TRAVERSAL_TIME 
-    RERR_RATELIMIT = 10 
-    RING_TRAVERSAL_TIME = 2 * NODE_TRAVERSAL_TIME * (TTL_VALUE + TIMEOUT_BUFFER) 
-    RREQ_RATELIMIT = 10 
+    ACTIVE_ROUTE_TIMEOUT = 3000
+    NODE_TRAVERSAL_TIME = 40
+    MY_ROUTE_TIMEOUT = 2 * ACTIVE_ROUTE_TIMEOUT
+    NET_TRAVERSAL_TIME =2 * NODE_TRAVERSAL_TIME # * NET_DIAMETER
+    PATH_DISCOVERY_TIME = 2 * NET_TRAVERSAL_TIME
+    RREQ_RETRIES = 2
 
-    def isMSGStale(currentEntry, newEntry):
-        if newEntry.flagOne:
-            return False
-        if currentEntry.destinationSeqNum == newEntry.destinationSeqNum and newEntry.hopCount+1 < currentEntry.hopCount:
-            return False
-        if newEntry.destinationSeqNum-currentEntry.destinationSeqNum < 0:
-            return True
+    def processRREQ(self, rreq: RouteRequest):
+        self.routingTable.updateEntryWithDestination(self.lastAdress)
+        for entry in self.rreqBuffer:
+            if (rreq.requestID,rreq.originatorAdress) == entry:
+                return
+        rreq.hopCount += 1
+        self.reverseRoutingTable.updateEntryWithRREQ(rreq,self.lastAdress)
+        if rreq.destinationAdress is not self.ownAdress or not self.routingTable.hasEntryForDestination(rreq.destinationAdress):
+            self.send("FFFF", rreq.encode())
+            return
+        self.generateRREP(rreq,self.lastAdress)
+        
+    
+    def processRREP(self, rrep: RouteReply):
+        self.routingTable.updateEntryWithDestination(self.lastAdress)
+        rrep.hopCount += 1
+        self.routingTable.updateEntryWithRREP(rrep,self.lastAdress)
+        destRoute = self.routingTable.getEntry(rrep.destinationAdress)
+        destRoute.precursers.append(destRoute.nextHop)
+        destRoute.lifetime = max(destRoute.lifetime, int(time*1000)+AODV.ACTIVE_ROUTE_TIMEOUT)
+        sourceRoute = self.reverseRoutingTable.getEntry(rrep.originatorAdress)
+        sourceRoute.precursers.append(sourceRoute.nextHop)
+        self.send(destRoute.nextHop,rrep.encode())
 
     def send(destinationAdress: str, msg: str):
         ...
 
-    def updateRoute(self, package):
-        #TODO create Update Route Method
+    def generateRREP(self, rreq: RouteRequest, previousAdress: str):
+        rrep = RouteReply()
+        if rreq.destinationAdress == self.ownAdress:
+            rrep.lifetime = AODV.MY_ROUTE_TIMEOUT
+            if self.sequenceNumber+1 == rreq.destinationSequence:
+                self.sequenceNumber += 1
+            rrep.destinationSequence = self.sequenceNumber
+            return
+        route = self.routingTable.getEntry(rreq.destinationAdress)
+        route.precursers.append(previousAdress)
+        reverseRoute = self.reverseRoutingTable.getEntry(rreq.originatorAdress)
+        reverseRoute.precursers = route.nextHop
+        self.routingTable.updateEntry(route)
+        self.reverseRoutingTable.updateEntry(reverseRoute)
+        rrep.destinationSequence = route.destinationSequenceNumber
+        rrep.hopCount = route.hopCount
+        rrep.lifetime = route.lifetime - int(time()*1000)
+        self.send(rreq.originatorAdress, rrep.encode())
+
+    def waitForResponse(self):
         ...
 
-    def generateRREP(self, RREQ: RouteRequest) -> bool:
-        if RREQ.destinationAdress == self.ownAdress:
-            if self.sequenceNumber+1 == RREQ.destinationSequence:
-                self.sequenceNumber = self.sequenceNumber+1
-            RREP = RouteReply()
-            RREP.destinationAdress = RREQ.destinationAdress
-            RREP.destinationSequence = self.sequenceNumber
-            RREP.lifetime = self.MY_ROUTE_TIMEOUT
-            self.serialPortClient.send(RREP.encode())
-            return True
-        if self.routingTable.hasEntryForDestination(RREQ.destinationAdress):
-            RREP = RouteReply()
-            entry = self.routingTable.getEntry(RREQ.destinationAdress)
-            entry.precursers.append(self.lastAdress)
-            reverseEntry = self.reverseRoutingTable.getEntry(RREQ.originatorAdress)
-            reverseEntry.precursers.append(entry.nextHop)
-            self.routingTable.updateEntry(entry)
-            self.reverseRoutingTable.updateEntry(reverseEntry)
-            RREP.destinationSequence = entry.destinationSeqNum
-            RREP.hopCount = entry.hopCount
-            RREP.lifetime = entry.lifetime #laut RFC auch minus current time ??
-            return True
-        return False
-
-    def processRREQ(self, RREQ: RouteRequest):
-        self.updateRoute(RREQ)
-        for package in self.rreqBuffer:
-            if RREQ.originatorAdress == package.originatorAdress and RREQ.requestID == package.requestID:
-                return
-        RREQ.hopCount = RREQ.hopCount+1
-        if self.reverseRoutingTable.hasEntryForDestination(RREQ.originatorAdress):
-            oldEntry = self.reverseRoutingTable.getEntry(RREQ.destinationAdress)
-            sequenceNumber = oldEntry.destinationSeqNum
-            if RREQ.originatorSequence > oldEntry.destinationSeqNum:
-                sequenceNumber = RREQ.originatorSequence
-            newEntry = Route(RREQ.destinationAdress,sequenceNumber, True, RREQ.hopCount, self.lastAdress, None, None)
-            self.reverseRoutingTable.updateEntry(newEntry)
-        else:
-            newEntry = Route(RREQ.destinationAdress,RREQ.originatorSequence, True, RREQ.hopCount, self.lastAdress, None, None)
-            self.reverseRoutingTable.addEntry(newEntry)
-        if not self.generateRREP(RREQ):
-            RREQ.hopCount = RREQ.hopCount+1
-            self.serialPortClient.send(RREQ.encode())
-        #6.5 Unterschied zwischen Update und create
-        #6.5 lifetime der Reverse Route
-        #6.5 Sequenz Nummer beim Updaten und weiterleiten des RREQ ?
-        
-
+    def generateRREQ(self, isSequenceNumberUnknown: bool, destinationSequenceNumber: str):
+        rreq = RouteRequest()
+        if isSequenceNumberUnknown:
+            rreq.unknownSequenceNumber = True
+        rreq.destinationSequence = destinationSequenceNumber
+        self.sequenceNumber += 1
+        rreq.originatorSequence = self.sequenceNumber
+        rreq.requestID = self.lastRequestID+1
+        self.rreqBuffer.append((rreq.requestID,rreq.originatorAdress))
+        self.send("FFFF", rreq.encode())
+        self.waitForResponse()
 
     def parse(self, msg:str):
         msgMatch = re.match("LR, ?[0-9A-F]{4}, ?[0-9A-F]{2}, ?", msg)
